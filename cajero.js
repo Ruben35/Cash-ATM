1af176d2a7ce0a4f528390b6a7591b381cb21582
Ruben35 <ruben111416@gmail.com>
2018-12-25 19:49:20 -0600

    Terminando funcionamiento

--- cajero.js
+++ cajero.js
@@ -1,10 +1,27 @@
 var caja=[];
+caja.push(new Billete(100,4));
 caja.push(new Billete(50,3));
 caja.push(new Billete(20,2));
 caja.push(new Billete(10,2));
 
-var dineroSolicitado=15;
-var dineroRecibido=entregarDinero(dineroSolicitado);
+var dineroSolicitado=0;
+var seEntrego=false;
+
+var button =document.getElementById('extraer');
+var resultado= document.getElementById("resultado");
+button.addEventListener("click",extraer);
+
+function extraer(){
+  resultado.innerHTML="";
+    dineroSolicitado=parseInt(document.getElementById('dinero').value);
+    dineroAEntregar=entregarDinero(dineroSolicitado);
+    if(seEntrego){
+      for(var e of dineroAEntregar){
+        if(e.cantidad !=0 )
+          resultado.innerHTML += e.cantidad + " billetes de $"+ e.valor + "<br/>";
+      }
+    }
+}
 
 function entregarDinero(dinero){
   var entregado=[];
@@ -22,9 +39,11 @@ function entregarDinero(dinero){
     }
   }
   if(dinero!=0){ //Valido si en el ciclo pasado me dió todo el dinero, si no, no lo despliega
-    alert("No se puede regresar el dinero");
+    resultado.innerHTML="No se puede regresar el dinero";
+    seEntrego=false;
     return 0;
   }else{
+    seEntrego=true;
     return entregado;
   }
 }
